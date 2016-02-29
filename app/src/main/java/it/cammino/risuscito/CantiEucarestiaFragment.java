06d452283c30f815503dc4a3ed985dea8be1f624
marbat87 <marbat87@outlook.it>
2016-02-29 10:09:59 +0100

    Puliza codice

--- app/src/main/java/it/cammino/risuscito/CantiEucarestiaFragment.java
+++ app/src/main/java/it/cammino/risuscito/CantiEucarestiaFragment.java
@@ -91,7 +91,7 @@ public class CantiEucarestiaFragment extends Fragment {
 //                BottomSheetHelper.shareAction(getActivity(), getDefaultIntent())
 //                        .title(R.string.share_by)
 //                        .show();
-                BottomSheetFragment bottomSheetDialog = BottomSheetFragment.newInstance(getString(R.string.share_by), getDefaultIntent());
+                BottomSheetFragment bottomSheetDialog = BottomSheetFragment.newInstance(getDefaultIntent());
                 bottomSheetDialog.show(getFragmentManager(), null);
             }
         });
