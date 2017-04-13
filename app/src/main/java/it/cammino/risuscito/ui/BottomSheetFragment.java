2c78e18c6b6ccaa31b934fb3b3caf3d25ae4fe5b
marcello.battain <marbat87@outlook.it>
2017-04-13 17:39:08 +0200

    Migrazione bottomsheetadapter a fastadapter

--- app/src/main/java/it/cammino/risuscito/ui/BottomSheetFragment.java
+++ app/src/main/java/it/cammino/risuscito/ui/BottomSheetFragment.java
@@ -18,10 +18,17 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.TextView;
 
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.IAdapter;
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import it.cammino.risuscito.R;
-import it.cammino.risuscito.adapters.BottomSheetAdapter;
+import it.cammino.risuscito.items.BottomSheetItem;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
 
@@ -52,6 +59,10 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         setStyle(DialogFragment.STYLE_NO_TITLE, 0);
     }
 
+    @BindView(R.id.sheet_title) TextView titleView;
+    @BindView(R.id.sheet_title_area) View titleArea;
+    @BindView(R.id.shareList) RecyclerView mRecyclerView;
+
 //    @Override
 //    public void onAttach(Context activity) {
 //        super.onAttach(activity);
@@ -65,22 +76,26 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        ButterKnife.bind(this, view);
 
         Boolean showTitle = getArguments().getBoolean("showTitle");
-        TextView titleView = (TextView) view.findViewById(R.id.sheet_title);
+//        TextView titleView = (TextView) view.findViewById(R.id.sheet_title);
         if (showTitle) {
 //            int title = getArguments().getInt("title");
             titleView.setText(getArguments().getInt("title"));
-            view.findViewById(R.id.sheet_title_area).setVisibility(View.VISIBLE);
+//            view.findViewById(R.id.sheet_title_area).setVisibility(View.VISIBLE);
+            titleArea.setVisibility(View.VISIBLE);
         }
         else {
-            view.findViewById(R.id.sheet_title_area).setVisibility(View.GONE);
+//            view.findViewById(R.id.sheet_title_area).setVisibility(View.GONE);
+            titleArea.setVisibility(View.GONE);
         }
 
         final Intent intent = getArguments().getParcelable("intent");
         PackageManager pm = getActivity().getPackageManager();
 
         final List<ResolveInfo> list = pm.queryIntentActivities(intent, 0);
+        final List<BottomSheetItem> mList = new ArrayList<>();
 
 //        Log.i("BottomSheetHelper", "list size: " + list.size());
 
@@ -100,32 +115,58 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         if (lastAppInfo != null)
             list.add(0, lastAppInfo);
 
+        for (ResolveInfo item: list)
+            mList.add(new BottomSheetItem().withItem(item));
+
 //        Log.i("BottomSheetHelper", "list size: " + list.size());
 
-        View.OnClickListener clickListener = new View.OnClickListener() {
+//        View.OnClickListener clickListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SharedPreferences.Editor editor = PreferenceManager
+//                        .getDefaultSharedPreferences(getActivity())
+//                        .edit();
+//                editor.putString("ULTIMA_APP_USATA", ((TextView) v.findViewById(R.id.app_package)).getText().toString());
+//                editor.apply();
+//
+//                ComponentName name = new ComponentName(((TextView) v.findViewById(R.id.app_package)).getText().toString(),
+//                        ((TextView) v.findViewById(R.id.app_name)).getText().toString());
+//                if (intent != null) {
+//                    Intent newIntent = (Intent) intent.clone();
+//                    newIntent.setComponent(name);
+//                    getActivity().startActivity(newIntent);
+//                    getDialog().dismiss();
+//                }
+//            }
+//        };
+
+        FastAdapter.OnClickListener<BottomSheetItem> mOnClickListener = new FastAdapter.OnClickListener<BottomSheetItem>() {
             @Override
-            public void onClick(View v) {
+            public boolean onClick(View view, IAdapter<BottomSheetItem> iAdapter, BottomSheetItem item, int i) {
                 SharedPreferences.Editor editor = PreferenceManager
                         .getDefaultSharedPreferences(getActivity())
                         .edit();
-                editor.putString("ULTIMA_APP_USATA", ((TextView) v.findViewById(R.id.app_package)).getText().toString());
+                editor.putString("ULTIMA_APP_USATA", item.getItem().activityInfo.packageName);
                 editor.apply();
 
-                ComponentName name = new ComponentName(((TextView) v.findViewById(R.id.app_package)).getText().toString(),
-                        ((TextView) v.findViewById(R.id.app_name)).getText().toString());
+                ComponentName name = new ComponentName(item.getItem().activityInfo.packageName, item.getItem().activityInfo.name);
                 if (intent != null) {
                     Intent newIntent = (Intent) intent.clone();
                     newIntent.setComponent(name);
                     getActivity().startActivity(newIntent);
                     getDialog().dismiss();
                 }
+                return true;
             }
         };
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.shareList);
-        BottomSheetAdapter adapter = new BottomSheetAdapter(getActivity(), list, clickListener);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+//        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.shareList);
+//        BottomSheetAdapter adapter = new BottomSheetAdapter(getActivity(), list, clickListener);
+        FastItemAdapter<BottomSheetItem> adapter = new FastItemAdapter<>();
+        adapter.add(mList);
+        adapter.withOnClickListener(mOnClickListener);
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
     }
