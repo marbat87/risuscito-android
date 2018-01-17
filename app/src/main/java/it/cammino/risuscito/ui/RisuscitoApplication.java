430e93e9ccd926acf1b151c382d77365cbd9d82c
marcello.battain <marbat87@outlook.it>
2018-01-17 14:59:04 +0100

    Ritorno a Picasso per numero molto inferiore di metodi inutili

--- app/src/main/java/it/cammino/risuscito/ui/RisuscitoApplication.java
+++ app/src/main/java/it/cammino/risuscito/ui/RisuscitoApplication.java
@@ -1,17 +1,13 @@
 package it.cammino.risuscito.ui;
 
-import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.multidex.MultiDexApplication;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.mikepenz.iconics.IconicsDrawable;
 import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
 import com.mikepenz.materialdrawer.util.DrawerImageLoader;
-import com.mikepenz.materialdrawer.util.DrawerUIUtils;
+import com.squareup.picasso.Picasso;
 
 import it.cammino.risuscito.R;
 import it.cammino.risuscito.database.RisuscitoDatabase;
@@ -19,63 +15,65 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class RisuscitoApplication extends MultiDexApplication {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+  @Override
+  public void onCreate() {
+    super.onCreate();
 
-        RisuscitoDatabase.getInstance(this);
+    RisuscitoDatabase.getInstance(this);
 
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                        .setDefaultFontPath("fonts/Roboto-Regular.ttf")
-                        .setFontAttrId(R.attr.fontPath)
-//                        .addCustomStyle(TextField.class, R.attr.textFieldStyle)
-                        .build()
-        );
+    CalligraphyConfig.initDefault(
+        new CalligraphyConfig.Builder()
+            .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+            .setFontAttrId(R.attr.fontPath)
+            //                        .addCustomStyle(TextField.class, R.attr.textFieldStyle)
+            .build());
 
-        //initialize and create the image loader logic
-        /*DrawerImageLoader.init(new AbstractDrawerImageLoader() {
-            @Override
-            public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
-                Picasso.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
-            }
-            @Override
-            public void cancel(ImageView imageView) {
-                Picasso.with(imageView.getContext()).cancelRequest(imageView);
-            }
-        });*/
+    // initialize and create the image loader logic
+    DrawerImageLoader.init(
+        new AbstractDrawerImageLoader() {
+          @Override
+          public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
+            Picasso.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+          }
 
-        //initialize and create the image loader logic
-        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
-            @Override
-            public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
-                Glide.with(imageView.getContext()).load(uri)
-                        .apply(new RequestOptions().placeholder(placeholder)).into(imageView);
-            }
-
-            @Override
-            public void cancel(ImageView imageView) {
-                Glide.with(imageView).clear(imageView);
-            }
-
-            @Override
-            public Drawable placeholder(Context ctx, String tag) {
-                //define different placeholders for different imageView targets
-                //default tags are accessible via the DrawerImageLoader.Tags
-                //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
-                if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
-                    return DrawerUIUtils.getPlaceHolder(ctx);
-                } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
-                    ThemeableActivity mActivity = (ThemeableActivity) ctx;
-                    return new IconicsDrawable(ctx).iconText(" ").backgroundColor(mActivity.getThemeUtils().primaryColor()).sizeDp(56);
-                } else if ("customUrlItem".equals(tag)) {
-                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
-                }
-
-                //we use the default one for
-                //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
-
-                return super.placeholder(ctx, tag);
-            }
+          @Override
+          public void cancel(ImageView imageView) {
+            Picasso.with(imageView.getContext()).cancelRequest(imageView);
+          }
         });
-    }
+
+    // initialize and create the image loader logic
+    /*DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+        @Override
+        public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
+            Glide.with(imageView.getContext()).load(uri)
+                    .apply(new RequestOptions().placeholder(placeholder)).into(imageView);
+        }
+
+        @Override
+        public void cancel(ImageView imageView) {
+            Glide.with(imageView).clear(imageView);
+        }
+
+        @Override
+        public Drawable placeholder(Context ctx, String tag) {
+            //define different placeholders for different imageView targets
+            //default tags are accessible via the DrawerImageLoader.Tags
+            //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+            if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+                return DrawerUIUtils.getPlaceHolder(ctx);
+            } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+                ThemeableActivity mActivity = (ThemeableActivity) ctx;
+                return new IconicsDrawable(ctx).iconText(" ").backgroundColor(mActivity.getThemeUtils().primaryColor()).sizeDp(56);
+            } else if ("customUrlItem".equals(tag)) {
+                return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
+            }
+
+            //we use the default one for
+            //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+
+            return super.placeholder(ctx, tag);
+        }
+    });*/
+  }
 }
