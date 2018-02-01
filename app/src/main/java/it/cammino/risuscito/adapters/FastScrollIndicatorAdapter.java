06a06cbc2f11a3b9277bcf97f908e46aea97219f
marcello.battain <marbat87@outlook.it>
2018-02-01 13:41:47 +0100

    reinserita libreria com.takisoft.fix:preference-v7-simplemenu fix per la posizione del fastscroll ridimensionamento altezza oggetti liste come da standard (72dp)

--- app/src/main/java/it/cammino/risuscito/adapters/FastScrollIndicatorAdapter.java
+++ app/src/main/java/it/cammino/risuscito/adapters/FastScrollIndicatorAdapter.java
@@ -1,15 +1,20 @@
 package it.cammino.risuscito.adapters;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.mikepenz.fastadapter.IItem;
 import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter;
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 
+import it.cammino.risuscito.R;
 import it.cammino.risuscito.items.SimpleItem;
 
 public class FastScrollIndicatorAdapter<Item extends IItem> extends FastItemAdapter<Item>
-    implements FastScrollRecyclerView.SectionedAdapter {
+    implements FastScrollRecyclerView.SectionedAdapter, FastScrollRecyclerView.MeasurableAdapter {
+
+  private final String TAG = getClass().getCanonicalName();
 
   private int mIndicator;
 
@@ -18,31 +23,12 @@ public class FastScrollIndicatorAdapter<Item extends IItem> extends FastItemAdap
     this.mIndicator = indicator;
   }
 
-  //    @Override
-  //    public String getCustomStringForElement(int position) {
-  //        IItem item = getItem(position);
-  //        if (item instanceof SimpleItem && ((SimpleItem) item).getTitle() != null) {
-  //            //based on the position we set the headers text
-  //            switch (mIndicator) {
-  //                case 0:
-  //                    return ((SimpleItem)
-  // item).getTitle().getText().toString().substring(0,1).toUpperCase();
-  //                case 1:
-  //                    return ((SimpleItem) item).getPage().getText().toString();
-  //                case 2:
-  //                    return String.valueOf(((SimpleItem) item).getNumSalmo());
-  //                default:
-  //                    return ((SimpleItem)
-  // item).getTitle().getText().toString().substring(0,1).toUpperCase();
-  //            }
-  //        }
-  //        return "";
-  //    }
-
   @NonNull
   @Override
   public String getSectionName(int position) {
-    IItem item = getItem(position);
+    Log.d(TAG, "getSectionName: position " + position);
+    IItem item = getAdapterItem(position);
+    Log.d(TAG, "getSectionName: " + ((SimpleItem) item).getTitle().getText());
     switch (mIndicator) {
       case 0:
         return ((SimpleItem) item).getTitle().getText().toString().substring(0, 1).toUpperCase();
@@ -53,5 +39,12 @@ public class FastScrollIndicatorAdapter<Item extends IItem> extends FastItemAdap
       default:
         return ((SimpleItem) item).getTitle().getText().toString().substring(0, 1).toUpperCase();
     }
+  }
+
+  @Override
+  public int getViewTypeHeight(RecyclerView recyclerView, int viewType) {
+    return recyclerView
+        .getResources()
+        .getDimensionPixelSize(R.dimen.myListPreferredItemHeightLarge);
   }
 }
