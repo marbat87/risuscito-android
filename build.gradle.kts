e1a628b7f9d79e21a512c96a82c170ee4923039e
marcello.battain <marbat87@outlook.it>
2018-11-15 11:57:48 +0100

    ritorno a gradle 4.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.2.1")
         classpath("com.google.gms:google-services:4.2.0")
-        classpath(kotlin("gradle-plugin", version = "1.3.0"))
+        classpath(kotlin("gradle-plugin", version = "1.3.10"))
         classpath("io.fabric.tools:gradle:1.26.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
