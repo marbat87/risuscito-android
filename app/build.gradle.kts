9d72e15fbc398c4a243b78a7b9c5302efe30f94c
marcello.battain <marbat87@outlook.it>
2019-04-03 16:06:01 +0200

    library updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,9 +53,9 @@ val supportVersion = "1.0.0"
 val preferenceFixVersion = "1.0.0"
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
-    implementation("com.afollestad.material-dialogs:core:2.3.0")
-    implementation("com.afollestad.material-dialogs:input:2.3.0")
-    implementation("com.afollestad.material-dialogs:files:2.3.0")
+    implementation("com.afollestad.material-dialogs:core:2.6.0")
+    implementation("com.afollestad.material-dialogs:input:2.6.0")
+    implementation("com.afollestad.material-dialogs:files:2.6.0")
     implementation("me.zhanghai.android.materialprogressbar:library:1.6.1")
     implementation("com.mikepenz:materialdrawer:6.1.2")
     implementation("com.mikepenz:crossfader:1.6.0")
@@ -72,7 +72,7 @@ dependencies {
     implementation("com.takisoft.preferencex:preferencex-colorpicker:$preferenceFixVersion")
     implementation("com.google.android.gms:play-services-auth:16.0.1")
     implementation("com.google.firebase:firebase-core:16.0.8")
-    implementation ("com.google.firebase:firebase-firestore:18.1.0")
+    implementation ("com.google.firebase:firebase-firestore:18.2.0")
     implementation ("com.google.firebase:firebase-storage:16.1.0")
     implementation ("com.google.firebase:firebase-auth:16.2.0")
     implementation("com.crashlytics.sdk.android:crashlytics:2.9.9@aar") { isTransitive = true }
