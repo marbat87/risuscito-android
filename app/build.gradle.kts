a47086ca89a0257ca666aaa0fe556ffce196f12e
marcello.battain <marbat87@outlook.it>
2019-04-29 10:06:29 +0200

    nuovo canto Inno a Cristo Luce (IT - UK)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdkVersion(16)
         targetSdkVersion(28)
         multiDexEnabled = true
-        versionCode = 4406
+        versionCode = 4407
         versionName = "4.4.0"
     }
 
