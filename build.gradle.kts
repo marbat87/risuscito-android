9060d17dbeb50f6fa8df93bc1226caf847fb75cf
marcello.battain <marbat87@outlook.it>
2019-05-24 11:44:47 +0200

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         maven(url = "https://maven.fabric.io/public")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("com.android.tools.build:gradle:3.4.1")
         classpath("com.google.gms:google-services:4.2.0")
         classpath(kotlin("gradle-plugin", version = "1.3.31"))
         classpath("io.fabric.tools:gradle:1.28.0")
