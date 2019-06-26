c652ef7af19b8f387969f194c10c9e0af1891d92
marcello.battain <marbat87@outlook.it>
2019-06-26 18:06:50 +0200

    v4414

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdkVersion(16)
         targetSdkVersion(29)
         multiDexEnabled = true
-        versionCode = 4413
+        versionCode = 4414
         versionName = "4.4.0"
     }
 
