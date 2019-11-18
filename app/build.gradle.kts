0e79f7b8346af0bbf67e938f98beadc2706c3347
marbat87 <marbat87@outlook.it>
2019-11-18 15:52:09 +0100

    v4437

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdkVersion(16)
         targetSdkVersion(29)
         multiDexEnabled = true
-        versionCode = 4436
+        versionCode = 4437
         versionName = "4.4.0"
         vectorDrawables.useSupportLibrary = true
         javaCompileOptions {
