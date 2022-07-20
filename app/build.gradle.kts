70e83424a318705c8d0a0c5ee011404cca3634e3
m.battain <marbat87@oulook.it>
2022-07-20 14:08:10 +0200

    library firebase BOM update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.0")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.google.android.gms:play-services-auth:20.2.0")
-    implementation(platform("com.google.firebase:firebase-bom:30.2.0"))
+    implementation(platform("com.google.firebase:firebase-bom:30.3.0"))
     implementation("com.google.firebase:firebase-core")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
